db99e5572c1f03454534a50df4f5b8a430025285
吴志杰 <wuzhijie@variflight.com>
2020-01-02 11:00:30 +0800

    update

--- src/main/java/main.kt
+++ src/main/java/main.kt
@@ -1,14 +1,13 @@
 import io.ktor.application.call
 import io.ktor.application.install
-import io.ktor.routing.routing
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
-import io.ktor.features.gzip
 import io.ktor.features.Compression
+import io.ktor.features.gzip
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
 import io.ktor.routing.get
-import io.ktor.routing.post
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
 fun main() {
     var server = embeddedServer(Netty, 8081) {
