bc87882543d4f1ae77344845efbf8a6eb7a16f3d
吴志杰 <wuzhijie@variflight.com>
2020-01-02 10:54:54 +0800

    update

---
+++ src/main/java/main.kt
@@ -0,0 +1,25 @@
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.features.gzip
+import io.ktor.features.Compression
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.post
+
+fun main() {
+    var server = embeddedServer(Netty, 8081) {
+        install(Compression) {
+            gzip()
+        }
+        routing {
+            get("/") {
+                call.respondText("My Example Blog", ContentType.Text.Html)
+            }
+        }
+    }
+    server.start(wait = true)
+}
